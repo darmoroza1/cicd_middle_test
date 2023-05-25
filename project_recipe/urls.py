2a56b2c907d831d4d1802d88a5460799f1ad763b
Alina Darmoroz <daarmorozaa@gmail.com>
2023-05-25 09:52:22 +0300

    initial

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
+from recipe.views import main, category_list
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main, name='main'),
+    path('category', category_list, name='category_list')
+
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,22 @@
+from django.shortcuts import render
+from .models import Recipe, Category
+
+
+def main(request):
+    latest_recipes = Recipe.objects.order_by('-created_at')[:5]
+    context = {'latest_recipes': latest_recipes}
+    return render(request, 'main.html', context)
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    category_data = []
+
+    for category in categories:
+        recipe_count = Recipe.objects.filter(category=category).count()
+        category_data.append({'category': category, 'recipe_count': recipe_count})
+
+    context = {'category_data': category_data}
+    return render(request, 'category_list.html', context)
+
 
